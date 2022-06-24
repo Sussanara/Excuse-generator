32a6958c715315f869340e91159160d6d5625b12
Sussanara <sussanaracely@gmail.com>
2022-06-24 19:58:11 +0000

    excuse

--- src/app.js
+++ src/app.js
@@ -2,10 +2,8 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#excuse").innerHTML = randomNumber;
   console.log("Hello Rigo from the console!");
 };
