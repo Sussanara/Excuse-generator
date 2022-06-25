16ab0bbc83ce757da817626c10b5bdf9900ea749
Sussanara <sussanaracely@gmail.com>
2022-06-25 21:26:18 +0000

    excuse

--- src/app.js
+++ src/app.js
@@ -4,15 +4,32 @@ import "./style.css";
 
 window.onload = function() {
   let randomNumber = Math.random() * 10;
-  document.querySelector("#excuse").innerHTML = generateExcuse;
+  document.querySelector("#the-excuse").innerHTML = generateExcuse;
   console.log("Hello Rigo from the console!");
 };
 
 let generateExcuse = () => {
-  let pronoun = [];
-  let subject = [];
-  let action = [];
-  let where = [];
+  let pronoun = ["mi"];
+  let subject = ["mamá", "abuela", "hermana"];
+  let action = ["dio vuelta", "me quitó"];
+  let possetion = ["postre", "galleta"];
+  let where = ["en el almuerzo", "en la cena"];
 
-  return "No me dieron permiso";
+  let pronounIndex = Math.floor(Math.random() * pronounlength);
+  let subjectIndex = Math.floor(Math.random() * subjectlength);
+  let actionIndex = Math.floor(Math.random() * actionlength);
+  let possetionIndex = Math.floor(Math.random() * possetionlength);
+  let whereIndex = Math.floor(Math.random() * wherelength);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possetion[possetionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
