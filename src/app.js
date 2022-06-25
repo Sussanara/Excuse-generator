9893715c5d6dfb1b896f473a77d3883cc407121a
Sussanara <sussanaracely@gmail.com>
2022-06-25 19:42:11 +0000

    avance

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ import "./style.css";
 
 window.onload = function() {
   let randomNumber = Math.random() * 10;
-  document.querySelector("#excuse").innerHTML = randomNumber;
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse;
+  console.log("Hello Rigo from the console!" + randomNumber);
+};
+
+let generateExcuse = () => {
+  return "No me sonó la alarma";
 };
