7edca1d0267e98a55fddc3359ad9ddb1e93ac677
Sussanara <sussanaracely@gmail.com>
2022-06-26 03:38:50 +0000

    excuse

--- src/app.js
+++ src/app.js
@@ -3,23 +3,22 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
-  let randomNumber = Math.random() * 10;
-  document.querySelector("#the-excuse").innerHTML = generateExcuse;
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
 };
 
 let generateExcuse = () => {
-  let pronoun = ["mi"];
+  let pronoun = ["Mi", "La"];
   let subject = ["mamá", "abuela", "hermana"];
   let action = ["dio vuelta", "me quitó"];
   let possetion = ["postre", "galleta"];
   let where = ["en el almuerzo", "en la cena"];
 
-  let pronounIndex = Math.floor(Math.random() * pronounlength);
-  let subjectIndex = Math.floor(Math.random() * subjectlength);
-  let actionIndex = Math.floor(Math.random() * actionlength);
-  let possetionIndex = Math.floor(Math.random() * possetionlength);
-  let whereIndex = Math.floor(Math.random() * wherelength);
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possetionIndex = Math.floor(Math.random() * possetion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
 
   return (
     pronoun[pronounIndex] +
